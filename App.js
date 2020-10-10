78502c29b97c62c1261e64dbfdea3ae14e14e643
dashfuentes <smis117112@gmail.com>
2020-10-10 11:33:54 -0600

    styles and picker

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
-import React from 'react';
-import {  StyleSheet,ScrollView,View,Text} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {StyleSheet,Keyboard,View,Text,TouchableWithoutFeedback} from 'react-native';
 import Formulario from './componentes/Formulario';
 
 
 const App = () => {
+
+  
+  //ocultar teclado 
+  const ocultarTeclado = () =>{
+    Keyboard.dismiss();
+  }
   return (
     <>
-     <Formulario/>
+ <TouchableWithoutFeedback onPress={ () => ocultarTeclado() }>
+          <View style={styles.app}>
+              <View style={styles.contenido}>
+              
+                <Formulario 
+                 
+                />
+
+              </View>
+          </View>
+      </TouchableWithoutFeedback>
     </>
   );
 };
 
 const styles = StyleSheet.create({
- 
+  app: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#5a9597'
+  },
+  contenido: {
+    marginHorizontal: '2.5%'
+  }
 });
 
 export default App;
--- componentes/Formulario.js
+++ componentes/Formulario.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import { StyleSheet,ScrollView,View,Text,TextInput,TouchableWithoutFeedback} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet,ScrollView,View,Text,TextInput,TouchableWithoutFeedback,Animated} from 'react-native';
 import { Picker } from '@react-native-community/picker';
 const Formulario = () =>{
+    
+    const [ animacionboton ] = useState(new Animated.Value(1));
+
+   
+    const animacionEntrada = () => {
+         //console.log("entrando...")
+         //del tamanio inicial del boton se hara mas pequenio
+        Animated.spring( animacionboton, { 
+            toValue: .75
+        }).start();
+    }
+
+    const animacionSalida = () => {
+        //console.log("saliendo...")
+         
+        Animated.spring( animacionboton, { 
+            //se hara mas pequenio
+            toValue: 1, 
+            friction: 4,
+            tension: 30
+        }).start();
+    }
+    //convierto las animaciones en un array para pasarlo en el view
+    const estiloAnimacion = {
+        transform: [{ scale: animacionboton  }]
+    }
+
+
     return (
         <>
          <View style = {styles.Formulario}>
                 <TextInput
+                style = {styles.input}
                 placeholder = "Ciudad"
                 placeholderTextColor = "#666"
                 />
 
             <View>
-                <Picker>
+                <Picker
+                 itemStyle={{ height: 120, backgroundColor: '#FFF' }}
+                >
                     <Picker.Item label = "--Seleccione un pais--" value=""/>
                     <Picker.Item label="El Salvador" value="SV" />
                     <Picker.Item label="Estados Unidos" value="US" />
@@ -23,10 +54,19 @@ const Formulario = () =>{
                         <Picker.Item label="Perú" value="PE" />
                     </Picker>    
             </View>    
-            <TouchableWithoutFeedback>
-                  <View>
-                      <Text>Buscar Clima</Text>
-                  </View>
+            <TouchableWithoutFeedback
+            //cuando presionas
+            onPressIn={ () => animacionEntrada() }
+            //cuando dejas de presionar
+         onPressOut={ () => animacionSalida()  }
+
+>                       <Animated.View
+                        style={[ styles.btnBuscar, estiloAnimacion] }
+                    >
+                         <Text style = {styles.textoBuscar}>Buscar Clima</Text>
+                    </Animated.View>
+                     
+                  
             </TouchableWithoutFeedback>
          </View>
         </>
@@ -34,9 +74,28 @@ const Formulario = () =>{
 }
 
 const styles = StyleSheet.create({
- Formulario: {
-     marginTop: 100
- }
+    input: {
+        padding: 10,
+        height: 50,
+        backgroundColor: 'aliceblue',
+        fontSize: 20,
+        marginBottom: 20,
+        textAlign: 'center'
+    },
+    btnBuscar: {
+        marginTop: 50,
+        backgroundColor: '#000',
+        padding: 10,
+        justifyContent: 'center'
+    },
+    textoBuscar: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        textAlign: 'center',
+        fontSize: 18
+    }
+
 });
 
 export default Formulario;
